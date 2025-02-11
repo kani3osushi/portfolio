d357ecbb9bf44feef5491120627d3606927d4a15
kani3osushi <zakkubase5@icloud.com>
2025-02-11 17:17:54 +0900

    fix code

--- coffee-project/src/components/client-favorite.tsx
+++ coffee-project/src/components/client-favorite.tsx
@@ -1,20 +1,18 @@
 "use client";
 import React from "react";
-import { useFavorites } from "@/store/FavoritesContext";
 
 function FavoritesClient() {
-  const { favorites } = useFavorites();
-
   return (
     <ul>
-      {favorites.length === 0 ? (
+      <li>お気に入りがありません。</li>
+      {/* {favorites.length === 0 ? (
         <li>お気に入りがありません。</li>
       ) : (
         favorites.map((id) => (
           <li key={id}>{id}</li>
           // <CardItem key={id} />
         ))
-      )}
+      )} */}
     </ul>
   );
 }
